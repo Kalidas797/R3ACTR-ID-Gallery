bec763f7da4f64e18378d2cf094423acb3417ace
Kalidas797 <kalidasvs797@gmail.com>
2026-09-12 13:36:14 +0530

    svg

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import SocialLinks from './components/SocialLinks';
 import Lanyard from './components/Lanyard/Lanyard';
 import './App.css';
 
+// EDIT THIS CONSTANT TO CHANGE YOUR ID CARD IMAGE
+// Both .png and .svg are fully supported! (For SVG, ensure it has width/height attributes in the file)
+const ID_CARD_IMAGE = "/g483.svg";
+
 function App() {
   const [isMobile, setIsMobile] = useState(false);
 
@@ -16,13 +20,15 @@ function App() {
   return (
     <div className="app-container">
       {/* Brand header */}
-      <header className="brand-header">R.3.A.C.T.R</header>
+      <header className="brand-header">
+        <img src="/g458.svg" alt="R.3.A.C.T.R Logo" className="brand-logo" />
+      </header>
 
       {/* 3D Lanyard section */}
       <Lanyard
         position={[0, 0, isMobile ? 19 : 12]} // Dynamic zoom: slightly further back on mobile so it fits the narrow screen
         gravity={[0, -40, 0]}
-        frontImage="/id-card.png"
+        frontImage={ID_CARD_IMAGE}
         imageFit="contain" // Using contain to not crop the ID card image
         lanyardWidth={1}
       />
